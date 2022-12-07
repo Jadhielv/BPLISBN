28e1994cd77f3e08720449404d20579e3d196808
Jadhiel Vélez <jadhielv@gmail.com>
2022-12-07 05:11:50 -0400

    Add appsettings.json file

---
+++ BPLISBN/AppSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace BPLISBN
+{
+    public class AppSettings
+    {
+        public string? InputFolder { get; set; }
+        public string? OutputFolder { get; set; }
+        public string? APIEndpoint { get; set; }
+    }
+}
