97434df5337c63a763d550ef697e881440c5843b
Jadhiel Vélez <jadhielv@gmail.com>
2022-12-07 07:02:33 -0400

    Add GetEnumDescription extension method to Enums

---
+++ BPLISBN/Extensions/EnumExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+
+namespace BPLISBN.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string GetEnumDescription(this Enum? enumValue)
+        {
+            var fieldInfo = enumValue?.GetType().GetField(enumValue.ToString());
+            DescriptionAttribute[] descriptionAttributes = Array.Empty<DescriptionAttribute>();
+
+            if (fieldInfo != null)
+            {
+                descriptionAttributes = (DescriptionAttribute[])fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), false);
+            }
+
+            return descriptionAttributes.Length > 0 ? descriptionAttributes[0].Description : enumValue?.ToString();
+        }
+    }
+}
