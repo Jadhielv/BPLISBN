517fa620d5cd98ec039e8003fef2fbfcdeb0dd46
Jadhiel Vélez <jadhielv@gmail.com>
2022-12-07 08:35:19 -0400

    Refactor

--- BPLISBN/Program.cs
+++ BPLISBN/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BPLISBN.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace BPLISBN
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IHost host = CreateHostBuilder(args).Build();
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.Configure<AppSettings>(hostContext.Configuration.GetSection("AppSettings"));
+                    services.AddScoped<IReadContentService, ReadContentService>();
+                    services.AddScoped<IAPIConsumerService, APIConsumerService>();
+                    services.AddScoped<IFileConstructorService, FileConstructorService>();
+                });
+    }
+}
