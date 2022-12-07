f855dcb767b8b769294ee44f5eac062eb486cdaa
Jadhiel Vélez <jadhielv@gmail.com>
2022-12-07 04:13:52 -0400

    Add book context

---
+++ BPLISBN/Models/BookContext.cs
@@ -0,0 +1,13 @@
+﻿namespace BPLISBN.Models
+{
+    public class BookContext
+    {
+        public static readonly BookContext Instance = new BookContext();
+        public BookContext()
+        {
+            Books = new List<Book>();
+        }
+
+        public List<Book> Books { get; set; }
+    }
+}
