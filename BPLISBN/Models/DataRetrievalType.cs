8e0d6208d2248d281d3ba48f8238ed8af3c1a147
Jadhiel Vélez <jadhielv@gmail.com>
2022-12-07 03:44:07 -0400

    Add data retrieval type enum

---
+++ BPLISBN/Models/DataRetrievalType.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace BPLISBN.Models
+{
+    internal enum DataRetrievalType
+    {
+        [Description("Server")]
+        Server = 1,
+        [Description("Cache")]
+        Cache = 2
+    }
+}
