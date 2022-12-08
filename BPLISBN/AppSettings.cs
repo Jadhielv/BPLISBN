2537f5603bd4a0248c754514de83f7fe1b7b83d4
Jadhiel Vélez <jadhielv@gmail.com>
2022-12-08 02:00:46 -0400

    Remove unused class

--- BPLISBN/AppSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BPLISBN
-{
-    public class AppSettings
-    {
-        public string? InputFolder { get; set; }
-        public string? OutputFolder { get; set; }
-        public string? APIEndpoint { get; set; }
-    }
-}
